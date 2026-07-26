e796ae453f10be6ef89a74f90e0c079fd72f687a
rinconaline <aline.rincon@unifesp.br>
2026-07-26 00:41:02 -0300

    Footer e Header

--- server.js
+++ server.js
@@ -1,15 +1,31 @@
 const express = require('express');
-const path = require('path');
-
+const path = require('path'); // Declaração ÚNICA do path no topo
 const app = express();
 const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em: http://localhost:${PORT}`);
+});
+
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'pages', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em: http://localhost:${PORT}`);
+app.get('/sobre', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages', 'sobre.html'));
+});
+
+app.get('/produtos', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages', 'produtos.html'));
+});
+
+app.get('/galeria', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages', 'galeria.html'));
+});
+
+app.get('/contato', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages', 'contato.html'));
 });
